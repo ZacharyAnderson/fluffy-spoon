db92506ea0b3e58d0058bacaa87b503f5f02f463
Zach Anderson <zacha777@gmail.com>
2019-03-01 14:43:12 -0500

    movie flights problem

---
+++ movie_flights/movie_flights.py
@@ -0,0 +1,76 @@
+import unittest
+
+
+def can_two_movies_fill_flight(movie_lengths, flight_length):
+
+    # Determine if two movie runtimes add up to the flight length
+    # flight_length - Int in minutes
+    # movie_lengths - list of ints in minutes
+    # return true or false if two movies sum = flight_length 
+    # cannot have same movie twice
+    
+    remaining_time_set = set()
+    
+    for movie in movie_lengths:
+        second_movie_length = flight_length-movie
+        if second_movie_length in remaining_time_set:
+            return True
+        remaining_time_set.add(movie)
+
+    return False
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# Tests
+
+class Test(unittest.TestCase):
+
+    def test_short_flight(self):
+        result = can_two_movies_fill_flight([2, 4], 1)
+        self.assertFalse(result)
+
+    def test_long_flight(self):
+        result = can_two_movies_fill_flight([2, 4], 6)
+        self.assertTrue(result)
+
+    def test_one_movie_half_flight_length(self):
+        result = can_two_movies_fill_flight([3, 8], 6)
+        self.assertFalse(result)
+
+    def test_two_movies_half_flight_length(self):
+        result = can_two_movies_fill_flight([3, 8, 3], 6)
+        self.assertTrue(result)
+
+    def test_lots_of_possible_pairs(self):
+        result = can_two_movies_fill_flight([1, 2, 3, 4, 5, 6], 7)
+        self.assertTrue(result)
+
+    def test_not_using_first_movie(self):
+        result = can_two_movies_fill_flight([4, 3, 2], 5)
+        self.assertTrue(result)
+
+    def test_only_one_movie(self):
+        result = can_two_movies_fill_flight([6], 6)
+        self.assertFalse(result)
+
+    def test_no_movies(self):
+        result = can_two_movies_fill_flight([], 2)
+        self.assertFalse(result)
+
+
+unittest.main(verbosity=2)

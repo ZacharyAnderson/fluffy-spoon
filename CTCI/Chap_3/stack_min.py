c359e23e29d09134262c6bc8deeb37808ab8b496
Zach Anderson <zacha777@gmail.com>
2019-05-16 07:28:57 -0400

    Added min stack

---
+++ CTCI/Chap_3/stack_min.py
@@ -0,0 +1,32 @@
+class MinStack:
+
+    def __init__(self):
+        self.stack = list()
+        self.min = list()
+
+    def __repr__(self):
+        return repr(self.stack)
+
+    def push(self, value):
+        if not self.min or self.min[-1] > value:
+            self.min.append(value)
+        self.stack.append(value)
+
+    def pop(self):
+        popped_val = self.stack.pop()
+        if popped_val == self.min[-1]:
+            self.min.pop()
+
+    def min_val(self):
+        return self.min[-1]
+
+
+if __name__ == "__main__":
+    stack = MinStack()
+    stack.push(3)
+    stack.push(4)
+    print(stack.min_val())
+    stack.push(1)
+    print(stack.min_val())
+    stack.pop()
+    print(stack.min_val())

cf939cdb999bd6863f4f7b1d2adb3fe05a3b01c2
Zach Anderson <zacha777@gmail.com>
2019-05-18 14:50:27 -0400

    fixed sort stack

--- CTCI/Chap_3/sort_stack.py
+++ CTCI/Chap_3/sort_stack.py
@@ -7,22 +7,39 @@ class SortStack:
         return repr(self.stack)
 
     def push(self, value):
-        if self.is_empty():
-            temp_stack = list()
-            while self.stack:
-                if value > self.peek():
-                    temp_stack.append(self.stack.pop())
-                else:
-                    self.stack.append(value)
-                    while temp_stack:
-                        self.stack.append(temp_stack.pop())
-            # Write other cases
+        self.stack.append(value)
 
     def pop(self):
         return self.stack.pop()
 
     def peek(self):
-        return self.stack[-1]
+        if self.stack:
+            return self.stack[-1]
+        return -1
 
     def is_empty(self):
-        return len(self.stack) != 0
+        return len(self.stack) == 0
+
+
+def sort_stack(stack):
+    tmp_stack = SortStack()
+    while not stack.is_empty():
+        curr = stack.pop()
+        while not tmp_stack.is_empty() and tmp_stack.peek() > curr:
+            stack.push(tmp_stack.pop())
+        tmp_stack.push(curr)
+    return tmp_stack
+
+
+if __name__ == "__main__":
+    stack = SortStack()
+    stack.push(1)
+    stack.push(3)
+    stack.push(2)
+    print(stack)
+    stack.push(4)
+    stack.push(5)
+    print(stack)
+    stack.push(6)
+    print(stack)
+    print(sort_stack(stack))

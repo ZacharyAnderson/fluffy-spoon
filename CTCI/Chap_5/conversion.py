b1e2c54962a7ffdeb2a6ea437ae8f9c5a4fa834b
ZacharyAnderson <zacha777@gmail.com>
2019-05-22 10:54:40 -0400

    binary string conversion

---
+++ CTCI/Chap_5/conversion.py
@@ -0,0 +1,18 @@
+def conversion(integer1, integer2):
+    """
+        determines the number of bits you would need to flip
+        to convert integer1 into integer2
+    """
+    bit_difference = integer1 ^ integer2
+    count = 0
+    while bit_difference:
+        print(bin(bit_difference))
+        bit_difference ^= bit_difference & -bit_difference
+        count += 1
+    return count
+
+
+if __name__ == "__main__":
+    integer1 = 29
+    integer2 = 15
+    print(conversion(integer1, integer2))
